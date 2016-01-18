639a0fa1bd184256458b6b628b175813c52dfd4b
Alex Kravchenko <alex.krava@live.com>
2016-01-18 14:43:16 +0200

    Portfolio V1

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    minifyCss = require('gulp-minify-css');
+
+
+
+
+gulp.task('css', function () {
+  return gulp.src('app/style/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(autoprefixer({browsers: ['last 10 versions']}))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('app/css'))
+});
+
+// Сервер
+gulp.task('server', function () {  
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('app/style/*.css', ['css']);
+    gulp.watch([
+    'app/*.html',
+    'app/js/*.js',
+    'app/style/*.css'
+  ]).on('change', browserSync.reload);
+}
+);
+
+gulp.task('default', ['css', 'server', 'watch']);
